47a384fda82cf39bdc59f6bdb22998ca7d447620
Lucas Marinho <lucamarinho2012@hotmail.com>
2024-11-26 23:28:35 -0300

    Serviços

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Categorias from "./components/Categorias/Categorias";
 import Categorias2 from "./components/Categorias/Categorias2";
+import Servicos from "./components/Servicos/Servicos";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
         <Navbar />
         <Hero />
         <Categorias />
         <Categorias2 />
+        <Servicos />
     </div>
   );
 };
